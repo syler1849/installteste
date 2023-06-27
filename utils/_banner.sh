d33997053f3933bd40c1edbe37863daae0a9b126
syler1849 <syler1849@hotmail.com>
2023-06-27 17:38:25 -0300

    teste

---
+++ utils/_banner.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+  printf " © Astra Online - astraonline.com.br\n";
+  printf "${NC}";
+
+  printf "\n"
+}
